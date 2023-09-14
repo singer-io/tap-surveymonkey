3152c58bb6ee1d5915bce36ed5d5b0bf807cc9b2
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-09-14 16:56:55 +0000

    writing schema to catalog

--- tap_surveymonkey/__init__.py
+++ tap_surveymonkey/__init__.py
@@ -16,7 +16,7 @@ def main():
 
     # If discover flag was passed, run discovery mode
     if args.discover:
-        catalog = discover()
+        catalog = discover().dump()
     # Otherwise run in sync mode
     else:
         if args.catalog:
