fe5033c2e6433656e89ca72f62f5c389fd2b67d1
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-09-25 14:50:24 +0000

    removed key_properties from catalog_entry

--- tap_surveymonkey/discover.py
+++ tap_surveymonkey/discover.py
@@ -34,10 +34,10 @@ def discover():
         # make sure that the replication key field is mandatory
         if stream_object.replication_key:
             metadata.write(mdata, ("properties", stream_object.replication_key), "inclusion", "automatic")
+        
         catalog_entry = {
             "stream": stream_id,
             "tap_stream_id": stream_id,
-            "key_properties": stream_object.key_properties,
             "schema": schema,
             "metadata": metadata.to_list(mdata)
         }
