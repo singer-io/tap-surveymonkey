d56b958ca4ccfc9642686d8db8c3f12f51297ea1
Tushar Mittal <tushar.mittal@qlik.com>
2026-05-22 16:27:03 +0530

    [SAC-30903] Re-add urllib3==2.7.0 to install_requires (required for Dependabot CVE fix)

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     install_requires=[
         "singer-python==6.8.0",
         "requests==2.34.2",
-        "backoff==2.2.1"
+        "backoff==2.2.1",
+        "urllib3==2.7.0",
     ],
     extras_require={
         'dev': [
