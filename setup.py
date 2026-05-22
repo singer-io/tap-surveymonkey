19e4cbf9a9b8db6c9ac7b12247abd3312bc7d9f8
Tushar Mittal <tushar.mittal@qlik.com>
2026-05-22 14:32:23 +0530

    update requests

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     py_modules=["tap_surveymonkey"],
     install_requires=[
         "singer-python==6.8.0",
-        "requests==2.33.0",
+        "requests==2.34.2",
         "backoff==2.2.1",
     ],
     extras_require={
